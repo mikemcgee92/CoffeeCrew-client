8bdf9c81191d488268ecfe9824e70d85eee0af9a
Mike McGee <mmcgee738@gmail.com>
2025-08-21 21:03:26 -0500

    edited favicon, added tab title

--- src/app/page.js
+++ src/app/page.js
@@ -23,6 +23,8 @@ function Home() {
         margin: '0 auto',
       }}
     >
+      <title>CoffeeCrew</title>
+      <link rel="icon" href="favicon.ico" />
       {categories.map((category) => (
         <Button key={category.id} onClick={() => router.push(`/recipes?category_id=${category.id}`)}>
           {category.label}
