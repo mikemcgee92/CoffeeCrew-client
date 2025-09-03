f3eeb9bce858b4c9c5549f620f691b7294c296fc
Mike McGee <mmcgee738@gmail.com>
2025-09-02 20:53:54 -0500

    further styling

--- src/app/page.js
+++ src/app/page.js
@@ -38,45 +38,48 @@ function Home() {
     >
       <title>CoffeeCrew</title>
       <link rel="icon" href="favicon.ico" />
-      {categories.map((category) => (
-        <div key={category.id} className="button-row">
-          <Button className="btn btn-primary" key={category.id} onClick={() => router.push(`/recipes?category_id=${category.id}`)}>
-            {category.label}
-          </Button>
-          {user.uid === category.creator_id ? (
-            <>
-              <Button
-                className="btn btn-info"
-                onClick={() => {
-                  setCategoryLabel(category.label);
-                  setEditId(category.id);
-                  setEditMode(true);
-                  handleShowModal();
-                }}
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#434343">
-                  <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
-                </svg>
-              </Button>
-              <Button
-                className="btn btn-danger cat"
-                onClick={async () => {
-                  await deleteCategory(category.id, user.uid);
-                  getCategories().then(setCategories);
-                }}
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#434343">
-                  <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
-                </svg>
-              </Button>
-            </>
-          ) : (
-            ''
-          )}
-        </div>
-      ))}
+      <h1>Categories</h1>
+      <div className="categories-container">
+        {categories.map((category) => (
+          <div key={category.id} className="container-styled button-row">
+            <Button className="btn btn-primary" key={category.id} onClick={() => router.push(`/recipes?category_id=${category.id}`)}>
+              {category.label}
+            </Button>
+            {user.uid === category.creator_id ? (
+              <>
+                <Button
+                  className="btn btn-info"
+                  onClick={() => {
+                    setCategoryLabel(category.label);
+                    setEditId(category.id);
+                    setEditMode(true);
+                    handleShowModal();
+                  }}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#434343">
+                    <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
+                  </svg>
+                </Button>
+                <Button
+                  className="btn btn-danger cat"
+                  onClick={async () => {
+                    await deleteCategory(category.id, user.uid);
+                    getCategories().then(setCategories);
+                  }}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#434343">
+                    <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+                  </svg>
+                </Button>
+              </>
+            ) : (
+              ''
+            )}
+          </div>
+        ))}
+      </div>
       <div>
-        <Button className="btn btn-success" onClick={handleShowModal}>
+        <Button className="btn-new" onClick={handleShowModal}>
           + New Category
         </Button>
 
