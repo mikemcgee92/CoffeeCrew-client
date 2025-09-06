7bcccc398d4c9086875e775c23843270df40b2a5
Mike McGee <mmcgee738@gmail.com>
2025-09-06 13:59:09 -0500

    tickets loading every 3sec and completeable, reloads upon refresh though

--- src/app/ticketrail/page.js
+++ src/app/ticketrail/page.js
@@ -5,10 +5,14 @@ import getOrders from '../../utils/data/square_data';
 
 export default function TicketRail() {
   const [orders, setOrders] = useState([]);
+  const [completedOrders, setCompletedOrders] = useState([]);
+
+  const handleComplete = (order) => {
+    setCompletedOrders((prev) => [...prev, order]);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      console.warn(interval);
       getOrders().then(setOrders);
     }, 3000);
 
@@ -16,11 +20,29 @@ export default function TicketRail() {
   }, []);
 
   return (
-    <div>
-      {orders?.map((order) => {
-        const itemNames = order.line_items.map((lineItem) => lineItem.name);
-        return itemNames;
-      })}
-    </div>
+    <>
+      <title>Ticket Rail</title>
+      <div>
+        {orders?.map((order) => {
+          if (completedOrders?.some((completed) => completed.id === order.id)) {
+            return '';
+          }
+          const itemNames = order.line_items.map((lineItem) => lineItem.name);
+          const quantities = order.line_items.map((lineItem) => lineItem.quantity);
+          return (
+            <div key={order.id}>
+              {itemNames.map((itemName, index) => (
+                <p>
+                  {quantities[index]}x {itemName}
+                </p>
+              ))}
+              <button type="button" onClick={() => handleComplete(order)}>
+                Completed
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
