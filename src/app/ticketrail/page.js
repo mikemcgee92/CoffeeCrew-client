c2e78badf7e5c70bd7001bb48a178b12d9b566ed
Mike McGee <mmcgee738@gmail.com>
2025-09-13 10:25:38 -0500

    got a recipe from an order

--- src/app/ticketrail/page.js
+++ src/app/ticketrail/page.js
@@ -1,30 +1,31 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import { getOrders, getCompleteOrders, completeOrder, deleteCompletedOrder } from '../../utils/data/square_data';
+import { getRecipes } from '../../utils/data/recipe_data';
 
 export default function TicketRail() {
+  const router = useRouter();
   const [orders, setOrders] = useState([]);
-  // eslint-disable-next-line no-unused-vars
-  const [completedOrders, setCompletedOrders] = useState([]);
   const [completedIds, setCompletedIds] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
   const clearCompletedData = () => {
     if (completedIds.length >= 10) {
-      console.warn(completedIds[0]);
       deleteCompletedOrder(completedIds[0].order_id);
     }
   };
 
   const handleComplete = (order) => {
     completeOrder({ order_id: order.id });
-    setCompletedOrders((prev) => [...prev, order]);
     clearCompletedData();
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
       getOrders().then(setOrders);
+      getRecipes('').then(setRecipes);
       getCompleteOrders().then(setCompletedIds);
     }, 3000);
 
@@ -46,6 +47,13 @@ export default function TicketRail() {
               {itemNames.map((itemName, index) => (
                 <p>
                   {quantities[index]}x {itemName}
+                  {recipes?.some((recipe) => recipe.label === itemName) ? (
+                    <button type="button" onClick={() => router.push(`/recipes/${recipes.find((recipe) => recipe.label === itemName).id}`)}>
+                      Recipe
+                    </button>
+                  ) : (
+                    ''
+                  )}
                 </p>
               ))}
               <button type="button" onClick={() => handleComplete(order)}>
