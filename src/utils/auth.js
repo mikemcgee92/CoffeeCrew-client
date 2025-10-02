fae197ec99e53f68deb7177f437ae8fd8f3e34d0
Mike McGee <mmcgee738@gmail.com>
2025-10-02 16:21:41 -0500

    Posts to user info on sign in

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,12 +1,34 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import { clientCredentials } from './client';
 
-const signIn = () => {
+const postUserInfo = async (uid, userInfo) => {
+  await fetch(`${clientCredentials.databaseURL}/user-info`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: uid,
+    },
+    body: JSON.stringify(userInfo),
+  });
+};
+
+const signIn = async () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({
     prompt: 'select_account',
   });
-  firebase.auth().signInWithPopup(provider);
+
+  try {
+    const result = await firebase.auth().signInWithPopup(provider);
+    const { user } = result;
+    const info = {
+      display_name: user.displayName,
+    };
+    await postUserInfo(user.uid, info);
+  } catch (error) {
+    console.error('Error during sign in:', error);
+  }
 };
 
 const signOut = () => {
