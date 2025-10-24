d79889dc4d8796b11f4456651564f5ee35e2c0d0
Mike McGee <mmcgee738@gmail.com>
2025-10-24 18:47:54 -0500

    Managers only can edit, create, and delete now

--- src/app/recipes/new/page.js
+++ src/app/recipes/new/page.js
@@ -1,13 +1,33 @@
 'use client';
 
+import { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import RecipeForm from '../../../components/forms/RecipeForm';
+import getUserInfo from '../../../utils/data/userinfo_data';
 
 export default function NewRecipePage() {
+  const auth = firebase.auth();
+  const user = auth.currentUser;
+
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    getUserInfo(user.uid).then(setUserInfo);
+  }, [user.uid]);
+
   return (
     <div>
       <title>New Recipe</title>
-      <h1>New Recipe</h1>
-      <RecipeForm />
+      {userInfo.is_manager ? (
+        <>
+          <h1>New Recipe</h1>
+          <RecipeForm />
+        </>
+      ) : (
+        <h1>Not authorized</h1>
+      )}
     </div>
   );
 }
